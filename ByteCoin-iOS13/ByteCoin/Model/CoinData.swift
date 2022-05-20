f934f3eaf40ae8e4aab8c1a61ea357d00e41bc19
Emir Keles <34314693+burhanemirkeles@users.noreply.github.com>
2022-05-20 10:47:42 +0300

    byte coin replica project added

---
+++ ByteCoin-iOS13/ByteCoin/Model/CoinData.swift
@@ -0,0 +1,15 @@
+//
+//  CoinData.swift
+//  ByteCoin
+//
+//  Created by Emir Keles on 20.05.2022.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+struct CoinData:Codable{
+    
+    let rate:Double
+    
+}
